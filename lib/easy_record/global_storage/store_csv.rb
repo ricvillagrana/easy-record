609b7129e7d731be66706abadd48b235a5462ebe
Ricardo Villagrana <rvillagrana@protonmail.com>
2019-02-08 13:38:19 -0600

    add: add instance methods, save and destroy from csv

--- lib/easy_record/global_storage/store_csv.rb
+++ lib/easy_record/global_storage/store_csv.rb
@@ -3,37 +3,82 @@ module GlobalStorage
     require 'csv'
     require 'snake_camel'
 
-    def save_as_csv
+    def write_to_csv(headers, records)
       CSV.open(csv_filename, 'wb') do |csv|
-        csv << attrs
-        self.all.each do |record|
-          csv << attrs.map { |attr| record.send(attr) }
+        csv << headers
+        records.each do |record|
+          if record.is_a?(Array)
+            csv << record
+          else
+            csv << headers.map { |attr| record.send(attr) }
+          end
         end
       end
     end
 
+    def save_as_csv
+      write_to_csv(instance_headers, self.all)
+    end
+
     def load_from_csv
-      records = body.map { |record| Hash[headers.zip(record)] }
-      records.each do |record|
+      read_from_csv.each do |record|
         self.new(record)
       end
+    end
+
+    def read_from_csv
+      csv_body.map { |record| Hash[csv_headers.zip(record)] }
+    end
+
+    def save_record(record)
+      records = csv_body
+      index = csv_index_of(record)
+      if index
+        records[index] = record
+      else
+        records << record
+      end
+      write_to_csv(csv_headers, records)
+    end
+
+    def destroy_record(record)
+      records = csv_body
+      if records.delete_at(csv_index_of(record))
+        write_to_csv(csv_headers, records)
+      end
+    end
+
+    def csv_index_of(record)
+      csv_body.index(csv_body.find { |row| row[csv_headers.index('id')].to_i == record.id.to_i } )
     end
 
     def csv_exist?
       File.exist?(csv_filename)
     end
 
+    def csv_contains?(record)
+      csv_contains_id?(record.id)
+    end
+
+    def csv_cantains_id?(id)
+      csv_body.any? { |row| row[csv_headers.index('id')] == id }
+    end
+
     def csv_filename
       "./#{class_name.snakecase}.csv"
     end
 
+    def csv_raw
+      { headers: csv_headers, body: csv_body }
+    end
+
     private
 
-    def headers
+    def csv_headers
       csv_data.to_a.shift
     end
 
-    def body
+    def csv_body
       csv_data.to_a[1..-1]
     end
 
@@ -45,7 +90,7 @@ module GlobalStorage
       CSV.read(csv_filename, headers: true)
     end
 
-    def attrs
+    def instance_headers
       self.first.instance_variables.map { |var| var.to_s[1..-1] }
     end
   end
